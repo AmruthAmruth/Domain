8ab2990f701ae2535cfbc2fdc276ea3ea5da12f3
Amruth Shyju <amruthshyju@gmail.com>
2025-05-17 16:50:24 +0530

    hashtable

---
+++ DSA/CircularList/circularList.js
@@ -0,0 +1,71 @@
+
+
+class Node {
+    constructor(data){
+        this.data=data;
+        this.next=null
+    }
+}
+
+
+class CircularList{
+    constructor(){
+        this.head=null;
+    }
+
+
+    append(data){
+        const newNode = new Node(data);
+        if(!this.head){
+           this.head=newNode;
+           newNode.next=this.head
+            return;
+        }
+        let current = this.head;
+        while(current.next !== this.head){
+            current=current.next
+        }
+        current.next=newNode;
+        newNode.next=this.head
+
+    }
+
+
+    display(){
+        if(!this.head){
+            console.log("List is Empty");
+            return;
+        }
+        let current = this.head;
+        let str=""
+       do{
+         str += current.data + "-> " 
+         current=current.next
+       }while(current !== this.head)
+        console.log(str);
+        
+    }
+
+
+    isCircular(){
+        if(!this.head) return false
+        let current=this.head.next;
+        while(current !==null && current !== this.head){
+            current=current.next
+        }
+        return current===this.head
+    }
+
+
+}
+
+
+const List = new CircularList()
+
+List.append(1)
+List.append(2)
+List.append(3)
+List.append(4)
+List.append(5)
+List.display()
+console.log(List.isCircular());
--- DSA/LinkedList/implementation.js
+++ DSA/LinkedList/implementation.js
@@ -1,112 +1,154 @@
 
 
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null
+class Node{
+    constructor(data){
+        this.data=data;
+        this.next=null
     }
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null
+
+class LinkedList{
+    constructor(){
+        this.head=null
     }
 
-    append(data) {
-        const newNode = new Node(data);
-        if (!this.head) {
-            this.head = newNode;
-            return
-        }
-        let current = this.head;
-        while (current.next) {
-            current = current.next;
-        }
-        current.next = newNode
-    }
-
-
-    prepend(data){
-        const newNode = new Node(data);
+    append(data){
+        const newNode = new Node(data)
         if(!this.head){
-            this.head=newNode;
+            this.head=newNode
             return;
         }
-        newNode.next=this.head;
-        this.head=newNode
+        let current = this.head;
+        while(current.next){
+            current=current.next
+        }
+        current.next=newNode
     }
+
 
     display(){
         if(!this.head){
-            console.log("List is empty");
-            return;
+            console.log("List is Empty");
+            return
         }
         let current = this.head;
+        let result=""
         while(current){
-            console.log(current.data);
-            current= current.next;
+            result+= current.data +" -> "
+            current=current.next;
         }
+        console.log(result,"null");
+    }
+
+
+    insertAtBiggning(data){
+        const newNode = new Node(data);
+       if(!this.head){
+        this.head=newNode;
+        return;
+       }
+       newNode.next=this.head;
+       this.head=newNode
     }
 
 
     delete(data){
         if(!this.head){
-            console.log("List is Empty");
-            return;
+            console.log("list is empty");
+            return
         }
         if(this.head.data===data){
             this.head=this.head.next;
-            return;
+            return
+        }
+        let prev=null
+        let current = this.head;
+       
+        while(current && current.data !== data){
+            prev=current;
+            current=current.next
+        }
+      if(current){
+          prev.next=current.next;
+      }else{
+        console.log("Element not found");
+      }
+    }
+
+    deleteFromBigginig(){
+        if(!this.head){
+            console.log("List is empty");
+            return
         }
 
-        let current = this.head;
+        this.head=this.head.next;
 
-        while(current.next && current.next.data !== data){
+    }
+
+
+    deletedFromEnd(){
+        if(!this.head){
+            console.log("List is empty");
+            return;
+        }
+        let prev=this.head;
+        let current = this.head;
+        while(current.next){
+            prev=current;
+            current=current.next
+        }
+    prev.next=null
+    }
+
+    findLength(){
+        if(!this.head){
+            console.log("List is empty");
+            return
+        }
+        let current = this.head;
+        let count=0;
+        while(current){
+            count++
             current=current.next
         }
 
-        if(current.next){
-            current.next=current.next.next;
-        }else{
-            console.log("No data Found");
-            
-        }
+        console.log("Number of Nodes",count);
+        
     }
 
-    insertAtMiddle(data){
-        const newNode = new Node(data);
-        if(!this.head || !this.head.next){
-          newNode.next=this.head;
-          this.head=newNode;
-          return;
-        }
-        let fast = this.head;
-        let slow=this.head;
-        let prev=null;
 
+    findMiddileElement(){
+        if(!this.head){
+            console.log("List is empty");
+            return;
+        }
+        let fast=this.head;
+        let slow=this.head;
         while(fast && fast.next){
             fast=fast.next.next;
-            prev=slow
-            slow=slow.next;
+            slow=slow.next
         }
-
-        prev.next=newNode;
-        newNode.next=slow
+        console.log("Middile Element is",slow.data);
+        
     }
+
+
+
 
 
 }
 
 
-const List = new LinkedList();
+const List = new LinkedList()
 
-List.append(1)
 List.append(2)
 List.append(3)
 List.append(4)
 List.append(5)
-List.append(6)  
-List.prepend(0)
-List.delete(0)
-List.insertAtMiddle(100)
+List.append(6) 
+List.delete(9)
+List.insertAtBiggning(1)
 List.display()
+List.findLength()
+List.findMiddileElement()
